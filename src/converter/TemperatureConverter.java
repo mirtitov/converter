8e1cf3a8fc990d4f736eb2ff934f3be85185e896
mirtitov <bolgarshin@mail.ru>
2025-08-07 21:55:17 +0300

    свич и лямбда

--- src/converter/TemperatureConverter.java
+++ src/converter/TemperatureConverter.java
@@ -9,20 +9,27 @@ public class TemperatureConverter implements BaseConverter<TemperatureUnit> {
         if (fromUnit == null || toUnit == null) {
             throw new ConversionException("Invalid units provided for temperature conversion.");
         }
-        if (fromUnit == TemperatureUnit.CELSIUS && toUnit == TemperatureUnit.FAHRENHEIT) {
-            return value * 9 / 5 + 32;
-        } else if (fromUnit == TemperatureUnit.CELSIUS && toUnit == TemperatureUnit.KELVIN) {
-            return value + 273.15;
-        } else if (fromUnit == TemperatureUnit.FAHRENHEIT && toUnit == TemperatureUnit.CELSIUS) {
-            return (value - 32) * 5 / 9;
-        } else if (fromUnit == TemperatureUnit.FAHRENHEIT && toUnit == TemperatureUnit.KELVIN) {
-            return (value - 32) * 5 / 9 + 273.15;
-        } else if (fromUnit == TemperatureUnit.KELVIN && toUnit == TemperatureUnit.CELSIUS) {
-            return value - 273.15;
-        } else if (fromUnit == TemperatureUnit.KELVIN && toUnit == TemperatureUnit.FAHRENHEIT) {
-            return (value - 273.15) * 9 / 5 + 32;
-        } else {
-            throw new ConversionException("Unsupported temperature conversion.");
-        }
+
+        return switch (fromUnit) {
+            case CELSIUS -> switch (toUnit) {
+                case FAHRENHEIT -> value * 9 / 5 + 32;
+                case KELVIN -> value + 273.15;
+                case CELSIUS -> value;
+                default -> throw new ConversionException("Unsupported conversion from Celsius");
+            };
+            case FAHRENHEIT -> switch (toUnit) {
+                case CELSIUS -> (value - 32) * 5 / 9;
+                case KELVIN -> (value - 32) * 5 / 9 + 273.15;
+                case FAHRENHEIT -> value;
+                default -> throw new ConversionException("Unsupported conversion from Fahrenheit");
+            };
+            case KELVIN -> switch (toUnit) {
+                case CELSIUS -> value - 273.15;
+                case FAHRENHEIT -> (value - 273.15) * 9 / 5 + 32;
+                case KELVIN -> value;
+                default -> throw new ConversionException("Unsupported conversion from Kelvin");
+            };
+            default -> throw new ConversionException("Unsupported temperature unit");
+        };
     }
 }
